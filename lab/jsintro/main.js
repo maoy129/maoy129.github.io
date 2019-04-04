7ddbf658cc21f723b5cf9c6695330240e1a4eb91
Youran Mao <maoy129@newchool.edu>
2019-04-04 14:00:05 -0400

    jsexercise

---
+++ lab/jsintro/main.js
@@ -0,0 +1,48 @@
+alert("Welcome! Please view console on your browser.");
+
+var a = "Youran Mao";
+console.log(a);
+
+var b = 1127;
+console.log(b);
+
+var c = "1127";
+console.log(c);
+
+var d = 27;
+console.log(b<d);
+
+console.log(a+" "+"likes"+" "+"number"+" "+c);
+
+console.log(1128-1);
+console.log(1100+27);
+console.log(2254/2);
+console.log(563.5*2);
+console.log(1127*2-1127);
+
+var countries = ["Japan", "Iceland", "Greece"];
+console.log(countries[0]);
+console.log(countries.length);
+
+var time = new Date ();
+console.log("The current time is"+" "+time);
+
+var currentHour = new Date().getHours();
+if(currentHour==11){
+	var x = "It is"+" "+11+" "+"now."
+} else{
+	var x = "It is "+"not"+" "+11+" "+"yet."
+}
+console.log(x);
+
+var n = a.length-1;
+console.log(n);
+if(n<=9){
+	var m = "My name is relatively short."
+} else if(n>14){
+	var m = "My name is relatively long."
+} else{
+	var m = "My name is neither short nor long."
+}
+console.log(m);
+
